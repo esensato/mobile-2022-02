5c3a42db41bc65c2334345d5b283cf3b4cf6dc51
Edson A. Sensato <esensato@hotmail.com>
2022-08-16 22:13:11 -0300

    16/08/2022

--- cloudant.js
+++ cloudant.js
@@ -1,52 +1,20 @@
 function main(params) {
+    
+    var auth = require('ibm-cloud-sdk-core');
+    var Cloudant = require('@ibm-cloud/cloudant');
+    
+    const authenticator = new auth.IamAuthenticator({
+        apikey: 'q6qJgMqMqgkCOVqsOUHg4gSm_Ui7P5_JmSSBnHCaNsQc',
+    });
+    
+    var client =  Cloudant.CloudantV1.newInstance( {authenticator: authenticator });
+    client.setServiceUrl('https://9927172e-6dc6-480b-bae7-fed88d632838-bluemix.cloudantnosqldb.appdomain.cloud');
 
-console.log('------> ', params.nome)
-
-var auth = require('ibm-cloud-sdk-core');
-var Cloudant = require('@ibm-cloud/cloudant');
-
-const authenticator = new auth.IamAuthenticator({
-  apikey: 'TEmnuQIVKJmv-dV-ydrjHvWBLOZ9Bmd89Jigq-lCQ8NT',
-});
-
-var cloudant =  Cloudant.CloudantV1.newInstance( {authenticator: authenticator });
-
-
-cloudant.setServiceUrl('https://246a1fed-5869-43e9-a60b-80d330e9efc0-bluemix.cloudantnosqldb.appdomain.cloud');
-const dbname = 'pizzaria';
-
-// create mydb database if it does not already exist
-
-    return new Promise((resolve, reject) => {
-
-cloudant.postDocument({ db: dbname, document: {nome: "teste"}})
-  .then(data => {
-    resolve(prepararRetorno({msg: "criado"}))
-  })
-  .catch(error => {
-    // ignore if database already exists
-    if (error.status === 412) {
-      reject(dbname + ' database already exists');
-    } else {
-        console.log(error)
-      reject('Error occurred when creating ' + dbname +
-      ' database', error.error);
-    }
-  });        
-
-
+    return new Promise((process, reject) => {
+        
+        client.postDocument({db: 'pedidos', document: {pizza: "4 Queijos"}})
+        .then((ok) => process({ body: ok }))
+        .catch((err) => reject({body: err}))
+        
     })
-
-}
-
-
-function prepararRetorno(mensagem, codigo) {
-
-   return {
-     body: mensagem,
-     statusCode: codigo,
-     headers:{ 'Content-Type': 'application/json'}
-   };
-  
-
 }

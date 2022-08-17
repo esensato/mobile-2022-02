27288bd38d072d94317f4e4a60cf7ffce97f3b47
Edson A. Sensato <esensato@hotmail.com>
2022-08-16 22:26:40 -0300

    16/08/2022

--- pizzaria/src/app/pedido/pedido.component.ts
+++ pizzaria/src/app/pedido/pedido.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -10,16 +11,44 @@ export class PedidoComponent implements OnInit {
 
   pizzaSelecionada: any
   bebidaSelecionada: any
+  quantidade = 1
+  total = 0
 
-constructor(private route: ActivatedRoute, private router: Router) {
+constructor(private route: ActivatedRoute, private router: Router, private http:HttpClient) {
   this.route.queryParams.subscribe(params => {
     if (this.router.getCurrentNavigation().extras.state) {
       this.pizzaSelecionada = this.router.getCurrentNavigation().extras.state.pizzaSelecionada;
       this.bebidaSelecionada = this.router.getCurrentNavigation().extras.state.bebidaSelecionada;
+      this.recalcular(1)
     }
   });
 }
 
   ngOnInit() {}
+
+  recalcular(novoValor) {
+    this.total = this.pizzaSelecionada.preco + this.bebidaSelecionada.preco * novoValor
+  }
+
+  queijoExtra(novoValor) {
+    if (novoValor === true) {
+      this.total += 10
+    } else {
+      this.total -= 10
+    }
+  }
+  
+  salvarPedido() {
+
+    let URL = 'https://us-south.functions.appdomain.cloud/api/v1/web/edson.sensato%40espm.br_dev/default/GravarPedido'
+
+    this.http.post(URL, {pedido: {pizza: this.pizzaSelecionada, 
+                                  bebida: this.bebidaSelecionada, 
+                                  quantidade: this.quantidade,
+                                  total: this.total}}).subscribe((ret) => {
+
+    })
+
+  }
 
 }

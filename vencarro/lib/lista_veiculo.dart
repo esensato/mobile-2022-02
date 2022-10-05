a817222420bf0764c096a5e846cf937b89271c1f
esensato <esensato@hotmail.com>
2022-10-04 21:33:41 -0300

    04/10/2022

--- vencarro/lib/lista_veiculo.dart
+++ vencarro/lib/lista_veiculo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:vencarro/Veiculo.dart';
+import 'package:vencarro/util.dart';
 
 class ListaVeiculos extends StatelessWidget {
   
@@ -8,9 +10,69 @@ class ListaVeiculos extends StatelessWidget {
     return MaterialApp(title: 'VenCarro',
       home: Scaffold(
         appBar: AppBar(title: Text('VenCarro')),
-        body: Center()
+        body: _ListaVeiculoStateful()
       ),
     );
   }
 
+}
+
+class _ListaVeiculoStateful extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ListaVeiculoState();
+}
+
+class _ListaVeiculoState extends State<_ListaVeiculoStateful>{
+
+  List<Veiculo> _listaVeiculos = List<Veiculo>.empty(growable: true);
+  var _veiculoSelecionado = Veiculo();
+
+  @override
+  void initState() {
+    super.initState();
+    // value = conteudo de Future<List<Veiculo>>
+    Util.obterMarcas().then((marcas) {
+      // indica que a interface grafica deve ser atualizada
+      setState(() {
+        _listaVeiculos.addAll(marcas);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ListView.builder(itemBuilder: (cts, idx){
+      return ListTile(title: Column(children: [Text(_listaVeiculos.elementAt(idx).marca),
+        Text(_listaVeiculos.elementAt(idx).modelo),
+        Text(_listaVeiculos.elementAt(idx).ano)],),
+      leading: const Icon(Icons.car_rental),
+      trailing: const Icon(Icons.arrow_right_outlined),
+      tileColor: idx % 2 == 0 ? Colors.blueAccent : Colors.white,
+      onTap: () {
+        // atualiza veiculo selecionado
+        _veiculoSelecionado = _listaVeiculos.elementAt(idx);
+
+        if (_veiculoSelecionado.idModelo == '') {
+          Util.obterModelos(_listaVeiculos.elementAt(idx).idMarca, _listaVeiculos.elementAt(idx).marca).then((modelos) {
+            setState(() {
+              _listaVeiculos.clear();
+              _listaVeiculos.addAll(modelos);
+            });
+          });
+        } else if (_veiculoSelecionado.idModelo != '' && _veiculoSelecionado.idAno == '') {
+          Util.obterAnos(_listaVeiculos.elementAt(idx).idMarca, _listaVeiculos.elementAt(idx).marca,
+              _listaVeiculos.elementAt(idx).idModelo, _listaVeiculos.elementAt(idx).modelo).then((anos) {
+            setState(() {
+              _listaVeiculos.clear();
+              _listaVeiculos.addAll(anos);
+            });
+          });
+        }
+
+      },);
+    },
+    itemCount: _listaVeiculos.length,);
+
+  }
 }
--- vencarro/lib/util.dart
+++ vencarro/lib/util.dart
@@ -14,4 +14,25 @@ class Util {
       return ret;
   }
 
+  static Future<List<Veiculo>> obterModelos(String idMarca, String marca) async {
+    var urlModelo = 'https://parallelum.com.br/fipe/api/v1/carros/marcas/${idMarca}/modelos';
+    print(urlModelo);
+    var ret = List<Veiculo>.empty(growable: true);
+    var res = await http.get(Uri.parse(urlModelo));
+    jsonDecode(res.body)['modelos'].forEach((item) {
+      ret.add(Veiculo(idMarca: idMarca, marca: marca, idModelo: item['codigo'].toString(), modelo: item['nome']));
+    });
+    return ret;
+  }
+
+  static Future<List<Veiculo>> obterAnos(String idMarca, String marca, String idModelo, String modelo) async {
+    var urlAno = 'https://parallelum.com.br/fipe/api/v1/carros/marcas/${idMarca}/modelos/${idModelo}/anos';
+    var ret = List<Veiculo>.empty(growable: true);
+    var res = await http.get(Uri.parse(urlAno));
+    jsonDecode(res.body).forEach((item) {
+      ret.add(Veiculo(idMarca: idMarca, marca: marca, idModelo: idModelo, modelo: modelo,
+                      idAno: item['codigo'], ano: item['nome']));
+    });
+    return ret;
+  }
 }
